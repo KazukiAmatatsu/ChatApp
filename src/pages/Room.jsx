02bea31070017b4967aeada7af5159ff9cd38705
KazukiAmatatsu <80532711+KazukiAmatatsu@users.noreply.github.com>
2021-05-29 13:13:22 +0900

    Update Room.jsx

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React from 'react';
+import firebase from '../firebase';
 
 // import Login from "./pages/Login";
 // import SignUp from "./pages/SignUp";
 
 const Room = () => {
+  const [messages, setMessages] = React.useState(null);
+  const [value, setValue] = React.useState('');
+
   return (
     <>
       <h1>Room</h1>
+      <ul>
+        <li>samle user : sample message</li>
+      </ul>
+      <form>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button type="submit">送信</button>
+      </form>
+      <button onClick={() => firebase.auth().signOut()}>Logout</button>
     </>
   );
 };
